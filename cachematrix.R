a2cb0a0d4fa128310213c8a04b9dfa2455d78084
zdilli <zd.classes@gmail.com>
2014-04-26 16:45:16 -0400

    Wrote in the matrix/inverse caching functions required by PA2.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    xinv <- matrix(, nrow = 0, ncol = 0)
+    set <- function(y){
+        x <<- y
+        xinv <<- matrix(, nrow = 0, ncol = 0)
+    }
+    get <- function() x
+    setinv <- function(inv) xinv <<- inv
+    getinv <- function() xinv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
@@ -12,4 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    xinv <- x$getinv()
+    if(is.numeric(xinv)) {
+        message("getting cached data")
+        return(xinv)
+    }
+    data <- x$get()
+    xinv <- solve(data, ...)
+    x$setinv(xinv)
+    xinv
 }
